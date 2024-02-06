62646edbdfcd9da4fb24724076997cc781e18abc
Anna <gstudentseo@gmail.com>
2024-02-06 23:57:53 +0100

    Created login using JWT

--- authController.js
+++ authController.js
@@ -1,7 +1,18 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const {validationResult} = require('express-validator')
+const {secret} = require("./config")
+
+const generateAccessToken = (id, roles) => {
+    const payload = {
+        id,
+        roles
+    }
+    return jwt.sign(payload, secret, {expiresIn: "24h"})
+
+}
 
 class authController {
     async registration (req, res){
@@ -15,7 +26,7 @@ class authController {
           if (candidate) {
             return res.status(400).json({message: "User with this name already exists"})
           }
-          const hashPassword = bcrypt.hashSync(password, 7);
+          const hashPassword = bcrypt.hashSync(password, 8);
           const userRole = await Role.findOne({value: "USER"})
           const user = new User({username, password: hashPassword, roles: [userRole.value]})
           await user.save()
@@ -28,7 +39,17 @@ class authController {
 
     async login (req, res) {
         try {
-
+            const {username, password} = req.body
+            const user = await User.findOne({username})
+            if (!user) {
+                return res.status(400).json({message: "User ${username} not found"})
+            }
+            const validPassword = bcrypt.compareSync(password, user.password)
+            if (!validPassword) {
+                return res.status(400).json({message: "Incorrect password"})
+            }
+            const token = generateAccessToken(user._id, user.roles)
+            return res.json({token})
         } catch (e) {
             console.log(e)
             res.status(400).json({message: "Login error"})
